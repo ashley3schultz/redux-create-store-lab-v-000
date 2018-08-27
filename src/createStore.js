c42890c710df87bd6db3c4a21517a7bae50228f7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-27 22:10:51 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,10 @@
 export default function createStore(reducer) {
 
+<<<<<<< HEAD
   let state = 0
+=======
+  let state
+>>>>>>> e4072b75b69116c6c5ab744b84bbc3cc5d8aef34
 
   function dispatch(action){
     state = reducer(state, action)
@@ -15,8 +19,23 @@ export default function createStore(reducer) {
     dispatch,
     getState
   }
+<<<<<<< HEAD
+=======
+}
+
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+>>>>>>> e4072b75b69116c6c5ab744b84bbc3cc5d8aef34
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+let store = createStore(changeCount) // createStore takes the changeCount reducer as an argument
+store.dispatch({ type: '@@INIT' });
